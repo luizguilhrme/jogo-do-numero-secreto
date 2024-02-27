b2f9b84820eb0f2d5083362408f36cee103b05c1
Luiz Guilherme <luiz7guilherme.lg@gmail.com>
2024-02-27 20:43:17 -0300

    usando sourcecontrol do vs code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e ' + numeroLimite);
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 15' + numeroLimite);
 }
 
 exibirMensagemInicial();
